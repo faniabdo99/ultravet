92bb137a7ea1e26b620df270e6d0c19b0501fca5
faniabdo99 <faniabdo99@gmail.com>
2022-11-21 17:18:18 +0200

    Favicon updates

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -5,10 +5,18 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>UltraVet - Your one stop shop for your pet needs</title>
-    <link rel="shortcut icon" href="{{url('public')}}/images/logo/favourite_icon.svg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{url('public')}}/css/all.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{url('public')}}/favicon-32x32.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{url('public')}}/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{url('public')}}/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{url('public')}}/favicon-16x16.png">
+    <link rel="manifest" href="{{url('public')}}/site.webmanifest">
+    <link rel="mask-icon" href="{{url('public')}}/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#ffffff">
     {{-- @vite(['resources/js/app.js']) --}}
 </head>
